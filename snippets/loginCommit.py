932e9c0808aa11d45923086c94b69f16c971e6d5
Moon.J.W <jsjs401@daum.net>
2016-11-12 03:46:29 +0900

    Modify loginCommit

--- snippets/loginCommit.py
+++ snippets/loginCommit.py
@@ -23,26 +23,26 @@ class EclassCheck:
         login_url='https://eclass.dongguk.edu/User.do?cmd=loginUser' # 로그인 검증 페이지
         params=urllib.parse.urlencode(login_info) # login_info를 바탕으로 Request 할 수 있게 변환
         req=urllib.request.Request(login_url,params.encode('utf-8')) # Request 생성. Request의 인자를 string 형식으로 직접보낼 수 없음. 'utf-8'로 인코딩 해야 함.
-        with urllib.request.urlopen(req) as response: # Request 전송
-            html = response.read()
-        #res = opener.open(login_url)
+        local_filename, headers = urllib.request.urlretrieve(req)
+        html = open(local_filename)
+        res = urllib.request.urlopen(req) # Request 전송
+        res = opener.open(login_url)
 
         #### LOGIN CHECK : user 이름 가져오기 ####
-        # main_url = 'https://eclass.dongguk.edu/Main.do?cmd=viewEclassMain&mainMenuId=menu_00050&subMenuId=&menuType=menu'
-        # res = opener.open(main_url)
-        # data = res.read().decode('utf-8')
-        # soup = BeautifulSoup(data, "html.parser")
-        return 'HI'
+        main_url = 'https://eclass.dongguk.edu/Main.do?cmd=viewEclassMain&mainMenuId=menu_00050&subMenuId=&menuType=menu'
+        res = opener.open(main_url)
+        data = res.read().decode('utf-8')
+        soup = BeautifulSoup(data, "html.parser")
 
-        # try :  # 로그인 실패시 예외처리
-        #         userName = soup.find('span', {'class': 'user'}).find('strong').text
-        #         userName = userName.strip()  # 양쪽 끝의 공백 문자 제거
-        #         #print(userName)
-        #         return userName
-        # except  AttributeError:
-        #         #print('입력하신 아이디 혹은 비밀번호가 일치하지 않습니다.')
-        #
-        #         return False
+        try :  # 로그인 실패시 예외처리
+                userName = soup.find('span', {'class': 'user'}).find('strong').text
+                userName = userName.strip()  # 양쪽 끝의 공백 문자 제거
+                #print(userName)
+                return userName
+        except  AttributeError:
+                #print('입력하신 아이디 혹은 비밀번호가 일치하지 않습니다.')
+
+                return False
 
 
         #logout_url = 'https://eclass.dongguk.edu/User.do?cmd=logoutUser'
