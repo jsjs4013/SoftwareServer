14e4e13837d7fa833636ae74bc27364331116be8
Moon.J.W <jsjs401@daum.net>
2016-11-11 23:14:25 +0900

    Modify urls and views

--- snippets/urls.py
+++ snippets/urls.py
@@ -63,9 +63,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^loginCheck/$', views.loginCommit),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^loginCheck/$', views.LoginCommit.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
