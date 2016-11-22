16ca0d83832a726ec48cce6e97cdc259ac9e90a2
Moon.J.W <jsjs401@daum.net>
2016-11-22 18:34:12 +0900

    Modify id pw

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -43,10 +43,9 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-    # password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
-        password = serializers.CharField(write_only=True)
 
         user = User.objects.create(
             username=validated_data['username']
