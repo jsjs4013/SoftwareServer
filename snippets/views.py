8e0646f41c0df46bd56a2d7f2999b990ae341763
Moon.J.W <jsjs401@daum.net>
2016-11-25 16:50:51 +0900

    Modify loginCheck

--- snippets/views.py
+++ snippets/views.py
@@ -39,7 +39,7 @@ class LoginCommit(APIView):
         ID = request.POST.get("ID", '')
         PW = request.POST.get("PW", '')
 
-        return Response((ID))
+        return Response((ID, PW))
 
         loginCheck = EclassCheck()
         userName = loginCheck.check(ID, PW)
