cc728d1cb730c7bd9cf79ab0cab65485bde137d5
Moon.J.W <jsjs401@daum.net>
2016-11-25 15:49:28 +0900

    Modify all print

--- snippets/views.py
+++ snippets/views.py
@@ -46,7 +46,7 @@ class LoginCommit(APIView):
         ID = received_json_data['ID']
         PW = received_json_data['PW']
 
-        return Response(PW)
+        return Response(received_json_data)
 
         loginCheck = EclassCheck()
         userName = loginCheck.check(ID, PW)
