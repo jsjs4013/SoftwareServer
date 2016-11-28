ff2b38e024907fc413969b6630f1328faecb126d
Moon.J.W <jsjs401@daum.net>
2016-11-28 22:13:06 +0900

    Modify login eclass to libary

--- snippets/loginCommit.py
+++ snippets/loginCommit.py
@@ -10,18 +10,19 @@ class EclassCheck:
         }
 
         # 실제 로그인 하는 부분. Dict 형태로 로그인 정보를 담아 request 보냄. Request 생성 시 두번째 인자가 들어오게 되면 자동으로 Post Request로 인식
-        login_url='https://eclass.dongguk.edu/User.do?cmd=loginUser' # 로그인 검증 페이지
+        login_url='http://lib.dongguk.edu/studyroom/mainFullView?type=pc' # 로그인 검증 페이지
         session = requests.session()
         r = session.post(login_url, data=login_info)
 
         #### LOGIN CHECK ####
-        main_url = 'https://eclass.dongguk.edu/Main.do?cmd=viewEclassMain&mainMenuId=menu_00050&subMenuId=&menuType=menu'
+        main_url = 'http://lib.dongguk.edu/studyroom/mainFullView?type=pc'
         r = session.get(main_url, timeout=5)
         data = r.content.decode('utf-8')
         soup = BeautifulSoup(data, "html.parser")
 
         try :  # 로그인 실패시 예외처리
-                userName = soup.find('span', {'class': 'user'}).find('strong').text
+                # userName = soup.find('span', {'class': 'user'}).find('strong').text
+                userName = soup.find('div', {'class': 'logout'}).find('span', {'class': 'name'}).text
                 userName = userName.strip()  # 양쪽 끝의 공백 문자 제거
                 session.close()
                 return userName  # user 이름 가져오기
--- snippets/views.py
+++ snippets/views.py
@@ -66,48 +66,16 @@ class LoginCommit(APIView):
         except User.DoesNotExist:
             return Response({'username':userName, 'overlap':0})
 
-import requests
-from bs4 import BeautifulSoup
 
 class TestLoginCommit(APIView):
     def get(self, request, my_parameter, my_parameters, format=None):
         ID = my_parameter
         PW = my_parameters
 
-        # loginCheck = EclassCheck()
-        # userName = loginCheck.check(ID, PW)
-        # del loginCheck
+        loginCheck = EclassCheck()
+        userName = loginCheck.check(ID, PW)
+        del loginCheck
         i = 0
-
-        login_info = {
-            'userDTO.userId': ID,  # YourID 2014112025
-            'userDTO.password': PW  # YourPW wlsduddl14!
-        }
-
-        # 실제 로그인 하는 부분. Dict 형태로 로그인 정보를 담아 request 보냄. Request 생성 시 두번째 인자가 들어오게 되면 자동으로 Post Request로 인식
-        login_url = 'https://eclass.dongguk.edu/User.do?cmd=loginUser'  # 로그인 검증 페이지
-        session = requests.session()
-        r = session.post(login_url, data=login_info)
-
-        #### LOGIN CHECK ####
-        main_url = 'https://eclass.dongguk.edu/Main.do?cmd=viewEclassMain&mainMenuId=menu_00050&subMenuId=&menuType=menu'
-        r = session.get(main_url, timeout=5)
-        data = r.content.decode('utf-8')
-        soup = BeautifulSoup(data, "html.parser")
-
-        try:  # 로그인 실패시 예외처리
-            userName = soup.find('span', {'class': 'user'}).find('strong').text
-            userName = userName.strip()  # 양쪽 끝의 공백 문자 제거
-            session.close()
-
-            try:
-                User.objects.get(username=ID)
-
-                return Response({'username': userName, 'overlap': 1})
-            except User.DoesNotExist:
-                return Response({'username': userName, 'overlap': 0})
-        except  AttributeError:
-            return Response('error')
 
         # while userName == False:
         #     loginCheck = EclassCheck()
