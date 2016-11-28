4d5a0db711007a8784237f1862b5ed9a68730138
Moon.J.W <jsjs401@daum.net>
2016-11-28 18:20:01 +0900

    Modify loginCheck2

--- snippets/loginCommit.py
+++ snippets/loginCommit.py
@@ -15,8 +15,8 @@ class EclassCheck:
         #### LOGIN OPERATION ####
         # 로그인 정보를 가지고 사이트를 돌아다녀야 하므로 opener에 쿠키 저장소를 설정해두고 로그인을 시도해야함
         cj = http.cookiejar.LWPCookieJar() # CookieJar() : 쿠키를 저장하는 곳
-        https_sslv23_handler = urllib.request.HTTPSHandler(context=ssl.SSLContext(ssl.PROTOCOL_SSLv23)) # python의 ssl protocol_SSLv23을 적용하기 위한 handler
-        opener = urllib.request.build_opener(https_sslv23_handler,urllib.request.HTTPCookieProcessor(cj)) # SSL과 Cookie를 사용하는 opener 생성. HTTPCookieProcessor : build_opener의 쿠키 저장소를 설정
+        # https_sslv23_handler = urllib.request.HTTPSHandler(context=ssl.SSLContext(ssl.PROTOCOL_SSLv23)) # python의 ssl protocol_SSLv23을 적용하기 위한 handler
+        opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj)) # SSL과 Cookie를 사용하는 opener 생성. HTTPCookieProcessor : build_opener의 쿠키 저장소를 설정
         urllib.request.install_opener(opener) # install_opener : 위에서 설정한 opener 설정을 전역적으로 사용
 
         # 실제 로그인 하는 부분. Dict 형태로 로그인 정보를 담아 request 보냄. Request 생성 시 두번째 인자가 들어오게 되면 자동으로 Post Request로 인식
