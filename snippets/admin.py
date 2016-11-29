120a278a92cfe6ba7536d03ce09954ce70a4f03c
Moon.J.W <jsjs401@daum.net>
2016-11-30 03:16:59 +0900

    Modify admin

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from snippets.models import User, UsedBook, Request
+from snippets.models import User, UsedBook, Request, ChatList
 
 admin.site.register(User)
 admin.site.register(UsedBook)
 admin.site.register(Request)
+admin.site.register(ChatList)
 
 # Register your models here.
