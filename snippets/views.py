5e9b7fb311249913f2aa6c0fe1c3573389604eb4
Moon.J.W <jsjs401@daum.net>
2016-12-06 15:00:37 +0900

    Modify sort10

--- snippets/views.py
+++ snippets/views.py
@@ -419,7 +419,7 @@ class SearchBook(APIView):
         received_json_data = json.loads(request.body.decode("utf-8"))
         bookName = received_json_data['bookName']
         # bookName = request.POST['bookName']
-        search = UsedBook.objects.filter(bookTitle__icontains=bookName).order_by('sellerPrice' * 1)
+        search = UsedBook.objects.filter(bookTitle__icontains=bookName).order_by('sellerPrice')
         # ordered = sorted(search, key=attrgetter('sellerPrice'))
         serializer = UsedBookSerializer(search, many=True)
 
